3e7addc4912ca1d7c5722f0fa6f7e397449a54ad
Rafouki <58076280+Rafouki@users.noreply.github.com>
2020-06-16 19:37:06 +0200

    Add files via upload

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sons;
+
+    void Awake()
+    {
+        foreach(Sound s in sons)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string nom)
+    {
+        Sound s = Array.Find(sons, son => son.nom == nom);
+        if (s == null)
+        {
+            Debug.LogWarning("Son : " + nom +" n'a pas été trouvé");
+            return;
+        }
+        s.source.Play();
+    }   
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using UnityEditor.ShaderGraph.Internal;
+
+[System.Serializable]
+public class Sound 
+{
+    public AudioClip clip;
+
+    public string nom;
+
+    [Range(0f,1f)]
+    public float volume;
+    [Range(.1f,3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
