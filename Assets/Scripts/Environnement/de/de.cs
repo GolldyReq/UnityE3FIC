4714f41017841292853dc70b99054edef37ad45a
Maissa Mezhoud <maissamezhoud@mac-m.home>
2020-06-19 22:56:22 +0200

    niveau bureau premiere etape

---
+++ Assets/Scripts/Environnement/de/de.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class de : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("OnCollisionEnter");
+
+        if (other.gameObject.CompareTag("Player"))  
+        {
+            Debug.Log("OnCollisionEnter with Player");
+                                                         
+            TextMesh m_text = GetComponentInChildren<TextMesh>();
+
+            if ( m_text.text == "9" )
+            {
+                Debug.Log(" 9 ");  
+                m_text.text = "0"  ;
+
+            }
+            else 
+            {               
+                m_text.text = (int.Parse(m_text.text) + 1).ToString() ;
+            }
+
+
+        }
+    }
+
+}
